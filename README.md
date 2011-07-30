ee17e1be8f7d2d2da51e720278bc8b1742fa3815
Chen Yufei <cyfdecyf@gmail.com>
2011-07-30 16:32:30 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+Why
+===
+
+Han Han writes great article about the Chinese society's problem. His blog is on
+Sina which does export full RSS. So I created this to convert the excerpt RSS to
+a FULL RSS.
+
+Thanks to [hpricot](https://github.com/hpricot/hpricot), this is really easy to do.
+
+I put this script on a web hosting server and use cron job to update the static
+full RSS file regular.
+
---
+++ fullrss.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+#encoding: UTF-8
+
+# Only tested with UTF-8 encoding XML and HTML
+# If there's problems with other encoding, convert to UTF-8 first
+
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
+# You can use this too
+def fetch_full_rss(source, content_fetcher)
+  feed = open(source) { |f| Hpricot.XML(f) }
+
+  item = feed/:item
+  item.each do |it|
+    link = (it/:link).inner_html
+    (it/:description).inner_html = content_fetcher.call(link)
+  end
+  feed
+end
+
+def fetch_sina_article(link)
+  doc = open(link) { |f| Hpricot(f) }
+
+  article = doc.search("//div[@id = 'articlebody']")
+  paras = article/('.articalContent p')
+  # use inner_text to conver html entities to character
+  content = paras.collect { |pa| pa.inner_html }.join("\n")
+end
+
+# Han Han's blog on Sina, I created this to read his great articles
+sources = [
+  ["http://blog.sina.com.cn/rss/1191258123.xml", "hanhan.xml", method(:fetch_sina_article)],
+]
+
+sources.each do |url, file, fetcher|
+  full = fetch_full_rss(url, fetcher)
+  File.open(file, "w") { |f| f.write(full) }
+end
+
