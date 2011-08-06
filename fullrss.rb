ab3861e8bd98c1cf5e0cc7c9f7b51ddd66ab0d9d
Chen Yufei <cyfdecyf@gmail.com>
2011-08-06 20:20:41 +0800

    Remove email and bookmark link in Solidot RSS.

--- fullrss.rb
+++ fullrss.rb
@@ -21,6 +21,10 @@ def add_xmlns_content(doc)
   end
 end
 
+def create_content(content)
+  return "\n<content:encoded><![CDATA[#{content}]]></content:encoded>"
+end
+
 def convert_rss(source, content_fetcher)
   feed = open(source) { |f| Hpricot.XML(f) }
   add_xmlns_content(feed)
@@ -40,16 +44,22 @@ def fetch_sina_article(item)
   paras = article/('.articalContent p')
   # use inner_text to conver html entities to character
   content = paras.collect { |pa| make_para(pa.inner_text) }.join("\n")
-  item.at(:description).after <<-CDATA
-  \n<content:encoded><![CDATA[#{content}]]></content:encoded>
-  CDATA
+  item.at(:description).after create_content(content)
+end
+
+# remove email and bookmark link
+def convert_solidot_article(item)
+  description = item.at(:description)
+  html = description.inner_html
+  id = html.index("&lt;img width='1' height='1' src='http://solidot.org.feedsportal.com/c/33236")
+  description.inner_html = html[0, id] if id
 end
 
 sources = [
   # Han Han's blog on Sina, I created this to read his great articles
   ["http://blog.sina.com.cn/rss/1191258123.xml", "hanhan.xml", method(:fetch_sina_article)],
-  #["http://solidot.org/index.rss", "solidot.xml", method(:fetch_solidot_article)],
-  #["./feed-hanhan.xml", "hanhan.xml", method(:fetch_sina_article)],
+  ["http://solidot.org/index.rss", "solidot.xml", method(:convert_solidot_article)],
+  #["./feed-solidot.xml", "solidot.xml", method(:convert_solidot_article)],
 ]
 
 #print fetch_sina_article('./article.html')
